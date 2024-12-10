bfd55633bf0966246e89f9cd1aea9c1ea1da8bfb
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2024-12-10 16:35:14 +0000

    add value/tiny type implementation

---
+++ ValueOf.Tests/ValueOfTest.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using FluentValidation;
+
+namespace ValueOf.Tests;
+
+public class ValueOfTest
+{
+    class Firstname(string value) : NonBlankString<Firstname>(value) { }
+
+    class Email(string value) : NonBlankString<Email>(value, new Validator())
+    {
+        private class Validator : AbstractValidator<string>
+        {
+            public Validator()
+            {
+                RuleFor(x => x)
+                    .SetValidator(new NonBlankStringValidator())
+                    .EmailAddress()
+                    .WithMessage("Email must be a valid email address");
+            }
+        }
+
+        public static explicit operator Email(string value) => new(value);
+    }
+
+    [Fact(DisplayName = "Equals and GetHashCode")]
+    public void EqualsAndHashCode()
+    {
+        Firstname name1 = new("John");
+        Firstname name2 = new("John");
+        Firstname name3 = new("Jane");
+
+        name1.Equals(name2).Should().BeTrue();
+        name1.Equals(name3).Should().BeFalse();
+        name1.GetHashCode().Should().Be(name2.GetHashCode());
+    }
+
+    [Theory(DisplayName = "Firstname should not be empty or blank")]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void ValidatesFirstname(string input)
+    {
+        Action act = () => new Firstname(input);
+        act.Should().Throw<ValidationException>();
+    }
+
+    [Theory(DisplayName = "Email should be validated")]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData("@")]
+    [InlineData("foo@bar.com@")]
+    public void ValidatesEmail(string input)
+    {
+        Action act = () => new Email(input);
+        act.Should().Throw<ValidationException>();
+    }
+
+    [Fact(DisplayName = "Implicit conversion")]
+    public void ImplicitConversion()
+    {
+        Firstname name = (Firstname)"John";
+
+        string raw = (string)name;
+
+        raw.Should().Be("John");
+    }
+}
---
+++ ValueOf/ValueOf.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation;
+
+namespace ValueOf;
+
+public abstract class ValueOf<T, TDerived>
+    where T : notnull
+    where TDerived : ValueOf<T, TDerived>
+{
+    public T Value { get; }
+
+    protected ValueOf(T value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        Value = value;
+    }
+
+    protected ValueOf(T value, AbstractValidator<T> validator)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        ArgumentNullException.ThrowIfNull(validator);
+        validator.ValidateAndThrow(value);
+        Value = value;
+    }
+
+    public R Transform<R>(Func<T, R> transform) => transform(Value);
+
+    public override bool Equals(object? obj) =>
+        obj is ValueOf<T, TDerived> other && EqualityComparer<T>.Default.Equals(Value, other.Value);
+
+    public override int GetHashCode() => Value.GetHashCode();
+
+    public override string ToString() => Value.ToString() ?? string.Empty;
+
+    public static implicit operator T(ValueOf<T, TDerived> valueOf) => valueOf.Value;
+
+    public static explicit operator ValueOf<T, TDerived>(T value) =>
+        (ValueOf<T, TDerived>)Activator.CreateInstance(typeof(TDerived), value)!;
+}
+
+public class NonBlankString<TDerived>(string value, AbstractValidator<string> validator)
+    : ValueOf<string, TDerived>(value, validator)
+    where TDerived : ValueOf<string, TDerived>
+{
+    public NonBlankString(string value)
+        : this(value, new NonBlankStringValidator()) { }
+}
+
+public class NonBlankStringValidator : AbstractValidator<string>
+{
+    public NonBlankStringValidator()
+    {
+        RuleFor(x => x)
+            .NotNull()
+            .WithMessage("String must not be null")
+            .NotEmpty()
+            .WithMessage("String must not be empty")
+            .Must(x => !string.IsNullOrWhiteSpace(x))
+            .WithMessage("String must not be whitespace");
+    }
+}
