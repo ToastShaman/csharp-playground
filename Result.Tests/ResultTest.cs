35d50e197fa61a74f18438464b3641cad4f77aee
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2024-12-11 13:31:48 +0000

    add result type

---
+++ Result.Tests/ResultTest.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+
+namespace Result.Tests;
+
+public class ResultTest
+{
+    [Fact(DisplayName = "Can map a success result")]
+    public void CanMapASuccessResult()
+    {
+        var result = IResult<int, string>.Success(42);
+        var mapped = result.Map(value => value * 2);
+        mapped.Should().BeOfType<Success<int, string>>();
+        mapped.GetOrDefault().Should().Be(84);
+    }
+
+    [Fact(DisplayName = "Can map a failure result")]
+    public void CanMapAFailureResult()
+    {
+        var result = IResult<int, string>.Failure("error");
+        var mapped = result.Map(value => value * 2);
+        mapped.Should().BeOfType<Failure<int, string>>();
+        mapped.GetOrDefault().Should().Be(0);
+    }
+
+    [Fact(DisplayName = "Can flat map a success result")]
+    public void CanFlatMapASuccessResult()
+    {
+        var result = IResult<int, string>.Success(42);
+        var mapped = result.FlatMap(value => IResult<int, string>.Success(value * 2));
+        mapped.Should().BeOfType<Success<int, string>>();
+        mapped.GetOrDefault().Should().Be(84);
+    }
+
+    [Fact(DisplayName = "Can flat map a failure result")]
+    public void CanFlatMapAFailureResult()
+    {
+        var result = IResult<int, string>.Failure("error");
+        var mapped = result.FlatMap(value => IResult<int, string>.Success(value * 2));
+        mapped.Should().BeOfType<Failure<int, string>>();
+        mapped.GetOrDefault().Should().Be(0);
+    }
+
+    [Fact(DisplayName = "Can fold a success result")]
+    public void CanFoldASuccessResult()
+    {
+        var result = IResult<int, string>.Success(42);
+        var folded = result.Fold(value => value * 2, error => 0);
+        folded.Should().Be(84);
+    }
+
+    [Fact(DisplayName = "Can fold a failure result")]
+    public void CanFoldAFailureResult()
+    {
+        var result = IResult<int, string>.Failure("error");
+        var folded = result.Fold(value => value * 2, error => 0);
+        folded.Should().Be(0);
+    }
+
+    [Fact(DisplayName = "Can swap a success result")]
+    public void CanSwapASuccessResult()
+    {
+        var result = IResult<int, string>.Success(42);
+        var swapped = result.Swap();
+        swapped.Should().BeOfType<Failure<string, int>>().Which.Unwrap().Should().Be(42);
+    }
+
+    [Fact(DisplayName = "Can swap a failure result")]
+    public void CanSwapAFailureResult()
+    {
+        var result = IResult<int, string>.Failure("error");
+        var swapped = result.Swap();
+        swapped.Should().BeOfType<Success<string, int>>().Which.Unwrap().Should().Be("error");
+    }
+
+    [Fact(DisplayName = "Can map a failure result on a failure")]
+    public void CanMapAFailureResultOnFailure()
+    {
+        var result = IResult<int, string>.Failure("error");
+        var mapped = result.MapFailure(error => error.Length);
+        mapped.Should().BeOfType<Failure<int, int>>().Which.Unwrap().Should().Be(5);
+    }
+
+    [Fact(DisplayName = "Can map a failure result on a failure")]
+    public void CanFlatMapAFailureResultOnFailure()
+    {
+        var result = IResult<int, string>.Failure("error");
+        var mapped = result.FlatMapFailure(error => IResult<int, string>.Success(error.Length));
+        mapped.Should().BeOfType<Success<int, string>>();
+        mapped.GetOrDefault().Should().Be(5);
+    }
+
+    [Fact(DisplayName = "Can run on success action")]
+    public void CanRunOnSuccessAction()
+    {
+        var actionExecuted = false;
+        var result = IResult<int, string>.Success(42);
+        var actioned = result.OnSuccess(value => actionExecuted = true);
+        actionExecuted.Should().BeTrue();
+    }
+
+    [Fact(DisplayName = "Can run on failure action")]
+    public void CanRunOnFailureAction()
+    {
+        var actionExecuted = false;
+        var result = IResult<int, string>.Failure("error");
+        var actioned = result.OnFailure(error => actionExecuted = true);
+        actionExecuted.Should().BeTrue();
+    }
+}
---
+++ Result/Result.cs
@@ -0,0 +1,121 @@
+﻿namespace Result;
+
+public interface IResult<S, F>
+{
+    public static IResult<S, F> Success(S value) => new Success<S, F>(value);
+
+    public static IResult<S, F> Failure(F error) => new Failure<S, F>(error);
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure { get; }
+
+    public IResult<S, F> OnSuccess(Action<S> action);
+
+    public IResult<S, F> OnFailure(Action<F> action);
+
+    public IResult<R, F> Map<R>(Func<S, R> transform);
+
+    public IResult<R, F> FlatMap<R>(Func<S, IResult<R, F>> transform);
+
+    public IResult<S, R> MapFailure<R>(Func<F, R> transform);
+
+    public IResult<S, R> FlatMapFailure<R>(Func<F, IResult<S, R>> transform);
+
+    public R Fold<R>(Func<S, R> ifSuccess, Func<F, R> ifFailure);
+
+    public IResult<F, S> Swap();
+
+    public S? GetOrDefault();
+
+    public S GetOrElse(S defaultValue);
+
+    public S GetOrElseGet(Func<S> action);
+}
+
+public class Success<S, F> : IResult<S, F>
+{
+    private readonly S _value;
+
+    public Success(S value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        _value = value;
+    }
+
+    public bool IsSuccess => true;
+
+    public bool IsFailure => false;
+
+    public S? GetOrDefault() => _value;
+
+    public IResult<R, F> Map<R>(Func<S, R> transform) => new Success<R, F>(transform(_value));
+
+    public IResult<R, F> FlatMap<R>(Func<S, IResult<R, F>> transform) => transform(_value);
+
+    public IResult<S, R> MapFailure<R>(Func<F, R> transform) => (IResult<S, R>)this;
+
+    public IResult<S, R> FlatMapFailure<R>(Func<F, IResult<S, R>> transform) => (IResult<S, R>)this;
+
+    public R Fold<R>(Func<S, R> ifSuccess, Func<F, R> ifFailure) => ifSuccess(_value);
+
+    public IResult<F, S> Swap() => new Failure<F, S>(_value);
+
+    public S Unwrap() => _value;
+
+    public IResult<S, F> OnSuccess(Action<S> action)
+    {
+        action(_value);
+        return this;
+    }
+
+    public IResult<S, F> OnFailure(Action<F> action) => this;
+
+    public S GetOrElse(S defaultValue) => _value;
+
+    public S GetOrElseGet(Func<S> action) => _value;
+}
+
+public class Failure<S, F> : IResult<S, F>
+{
+    private readonly F _value;
+
+    public Failure(F error)
+    {
+        ArgumentNullException.ThrowIfNull(error);
+        _value = error;
+    }
+
+    public bool IsSuccess => false;
+
+    public bool IsFailure => true;
+
+    public S? GetOrDefault() => default;
+
+    public IResult<R, F> Map<R>(Func<S, R> transform) => (IResult<R, F>)this;
+
+    public IResult<R, F> FlatMap<R>(Func<S, IResult<R, F>> transform) => (IResult<R, F>)this;
+
+    public IResult<S, R> MapFailure<R>(Func<F, R> transform) =>
+        new Failure<S, R>(transform(_value));
+
+    public IResult<S, R> FlatMapFailure<R>(Func<F, IResult<S, R>> transform) => transform(_value);
+
+    public R Fold<R>(Func<S, R> ifSuccess, Func<F, R> ifFailure) => ifFailure(_value);
+
+    public IResult<F, S> Swap() => new Success<F, S>(_value);
+
+    public F Unwrap() => _value;
+
+    public IResult<S, F> OnSuccess(Action<S> action) => this;
+
+    public IResult<S, F> OnFailure(Action<F> action)
+    {
+        action(_value);
+        return this;
+    }
+
+    public S GetOrElse(S defaultValue) => defaultValue;
+
+    public S GetOrElseGet(Func<S> func) => func();
+}
