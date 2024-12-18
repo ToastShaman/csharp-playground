9a292691272ef55fcfecdadd3f6dcb93f7e25828
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2024-12-18 11:09:49 +0000

    add option

--- WeatherForecast.API/Middleware/HttpContextLens.cs
+++ WeatherForecast.API/Middleware/HttpContextLens.cs
@@ -1,13 +1,16 @@
+using Optional;
+using Optional.Unsafe;
+
 namespace Middleware;
 
 public class HttpContextLens<T>
     where T : notnull
 {
-    private readonly Func<HttpContext, T> _get;
+    private readonly Func<HttpContext, Option<T>> _get;
 
     private readonly Action<HttpContext, T> _set;
 
-    public HttpContextLens(Func<HttpContext, T> get, Action<HttpContext, T> set)
+    public HttpContextLens(Func<HttpContext, Option<T>> get, Action<HttpContext, T> set)
     {
         ArgumentNullException.ThrowIfNull(get);
         ArgumentNullException.ThrowIfNull(set);
@@ -19,15 +22,14 @@ public class HttpContextLens<T>
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(key);
 
-        _get = context =>
-            context.Items[key] is T value
-                ? value
-                : throw new InvalidOperationException($"No value found for key {key}");
+        _get = context => context.Items[key] is T value ? Option.Some(value) : Option.None<T>();
 
         _set = (context, value) => context.Items[key] = value;
     }
 
-    public T Get(HttpContext context) => _get(context);
+    public T Get(HttpContext context) => _get(context).ValueOrFailure("Value not found");
+
+    public Option<T> Maybe(HttpContext context) => _get(context);
 
     public void Set(HttpContext context, T value) => _set(context, value);
 
