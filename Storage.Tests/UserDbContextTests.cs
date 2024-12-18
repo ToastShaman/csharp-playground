c29c48db3a88f43393e6cccf7771c48bd71e5211
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2024-12-18 15:51:35 +0000

    add ef core / database

---
+++ Storage.Tests/UserDbContextTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Storage.Tests;
+
+public class UserDbContextTests
+{
+    private static DbContextOptions<UserDbContext> GetInMemoryDbContextOptions() =>
+        new DbContextOptionsBuilder<UserDbContext>()
+            .UseInMemoryDatabase(Ulid.NewUlid().ToString())
+            .Options;
+
+    [Fact]
+    public async Task AddUser_ShouldAddUserToDatabase()
+    {
+        // Arrange
+        using var context = new UserDbContext(GetInMemoryDbContextOptions());
+
+        var user = new UserRecord(Guid.NewGuid(), "test@example.com", DateTime.UtcNow);
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        // Act
+        var storedUser = await context.Users.FirstOrDefaultAsync(u =>
+            u.Email == "test@example.com"
+        );
+
+        Assert.NotNull(storedUser);
+        Assert.Equal(user.Email, storedUser.Email);
+        Assert.Equal(user.CreatedAt, storedUser.CreatedAt);
+    }
+
+    [Fact]
+    public async Task DeleteUser_ShouldRemoveUserFromDatabase()
+    {
+        // Arrange
+        using var context = new UserDbContext(GetInMemoryDbContextOptions());
+        var user = new UserRecord(Guid.NewGuid(), "test@example.com", DateTime.UtcNow);
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        // Act
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+
+        // Assert
+        var storedUser = await context.Users.FirstOrDefaultAsync(u =>
+            u.Email == "test@example.com"
+        );
+        Assert.Null(storedUser);
+    }
+}
---
+++ Storage/UserDbContext.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Storage;
+
+public record UserRecord(Guid Id, string Email, DateTime CreatedAt);
+
+public class UserRecordValidator : AbstractValidator<UserRecord>
+{
+    public UserRecordValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.CreatedAt).NotNull();
+    }
+}
+
+#pragma warning disable CS8618
+
+public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+{
+    public DbSet<UserRecord> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<UserRecord>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+            entity.Property(r => r.Email).IsRequired();
+            entity.Property(r => r.CreatedAt).IsRequired();
+        });
+    }
+}
+
+#pragma warning restore CS8618
